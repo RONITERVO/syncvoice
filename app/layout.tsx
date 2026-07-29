7a4303885a84da745065617f84067839075591d0
Roni Tervo <ronitervo.rt@gmail.com>
2026-07-29 13:27:42 +0300

    Update Project mode social preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata(): Promise<Metadata> {
   const host = requestHeaders.get("x-forwarded-host") ?? requestHeaders.get("host") ?? "localhost:3000";
   const protocol = requestHeaders.get("x-forwarded-proto") ?? (host.startsWith("localhost") ? "http" : "https");
   const metadataBase = new URL(`${protocol}://${host}`);
-  const socialImage = new URL("/og.png", metadataBase).toString();
+const socialImage = new URL("/og-project.png", metadataBase).toString();
 
   return {
     metadataBase,
